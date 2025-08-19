58893b642f21201152c3d276332f46fad5b16ce5
tamsen <tamsen.dunn@gmail.com>
2025-08-19 13:19:52 -0700

    added rms plots for poplar..

--- modeling/rms_diffs_dgks_vs_empirical.py
+++ modeling/rms_diffs_dgks_vs_empirical.py
@@ -14,13 +14,45 @@ from modeling import ks_parsers
 
 class Final_DGKS_vs_Empirical(unittest.TestCase):
 
+    def test_poplar_final_plots(self):
+        # linux
+        demographiKS_out_path = '/home/tamsen/Data/DemographiKS_output_from_mesx/EmpiricalDataTesting_2/Poplar'
+        truth_out_path = '/home/tamsen/Data/DemographiKS_output_from_mesx/EmpiricalDataTesting_2/Poplar/Truth'
+
+        species_for_plot_title = 'EMP_Pop_07_and_half_11'
+        out_png = os.path.join(demographiKS_out_path, species_for_plot_title + '_final_poplar.png')
+        real_full_path = os.path.join(truth_out_path, 'poplar.ks.tsv')
+        real_ks_results = ks_parsers.parse_external_ksfile(real_full_path)
+
+        sim_full_path = os.path.join(demographiKS_out_path,
+                                     species_for_plot_title,
+                                     'allotetraploid_bottleneck.csv')
+        demographiKS_ks_results = read_Ks_csv(sim_full_path, False)
+        bin_size = 0.01
+        max_Ks = 0.4
+        bins = np.arange(bin_size, max_Ks + 0.1, bin_size)
+        hist_ys_real, bins_real, patches = plt.hist(real_ks_results, bins=bins, facecolor='b', alpha=0.25,
+                                                    density=True)
+
+        hist_ys_sim, bins_sim, patches = plt.hist(demographiKS_ks_results, bins=bins, facecolor='b', alpha=0.25,
+                                                  density=True)
+
+        hist_ys_1 = [hist_ys_real, bins_real]
+        hist_ys_2 = [hist_ys_sim, bins_sim]
+        list_of_hist_data = [hist_ys_1, hist_ys_2]
+
+        overlay_differences_in_curves(species_for_plot_title, list_of_hist_data, out_png)
+
+        self.assertEqual(True, True)  # add assertion here
+
     def test_maize_final_plots(self):
         # linux
         demographiKS_out_path = '/home/tamsen/Data/DemographiKS_output_from_mesx/EmpiricalDataTesting_2/Maize'
         truth_out_path = '/home/tamsen/Data/DemographiKS_output_from_mesx/EmpiricalDataTesting_2/Maize/Truth'
-        out_png = os.path.join(demographiKS_out_path, 'final2.png')
+
 
         species_for_plot_title = 'EMP_Mays_26_29_combined'
+        out_png = os.path.join(demographiKS_out_path, species_for_plot_title+ '_final_mays.png')
         real_full_path = os.path.join(truth_out_path, 'mays.ks.tsv')
         real_ks_results = ks_parsers.parse_external_ksfile(real_full_path)
 
@@ -51,10 +83,11 @@ class Final_DGKS_vs_Empirical(unittest.TestCase):
         demographiKS_out_path = '/home/tamsen/Data/DemographiKS_output_from_mesx/EmpiricalDataTesting_2/Coffee'
         truth_out_path = '/home/tamsen/Data/DemographiKS_output_from_mesx/EmpiricalDataTesting_2/Coffee/Truth'
 
-        out_png = os.path.join(demographiKS_out_path, 'final_coffee.png')
+
 
         #species_for_plot_title = 'EMP_Coff_36_m07d09y2025_h12m22s40'
         species_for_plot_title = 'EMP_Coff_35_m07d01y2025_h08m58s51'
+        out_png = os.path.join(demographiKS_out_path,species_for_plot_title + '_final_coffee.png')
         real_full_path = os.path.join(truth_out_path, 'coffea.ks.tsv')
         real_ks_results = ks_parsers.parse_external_ksfile(real_full_path)
 
