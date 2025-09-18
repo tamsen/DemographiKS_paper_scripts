d3f52fb299dcb1145348f00e3d0b193caa204b71
tamsen <tamsen.dunn@gmail.com>
2025-09-18 09:50:07 -0700

    adding more birth,death model stuff

--- modeling/birth_and_death_with_escape.py
+++ modeling/birth_and_death_with_escape.py
@@ -1,11 +1,20 @@
-import scipy.stats as st
+import math
 
-#https://scicomp.stackexchange.com/questions/1658/define-custom-probability-density-function-in-python
-class my_pdf(st.rv_continuous):
-    def _pdf(self,x):
-        return 3*x**2  # Normalized over its range, in this case [0,1]
+import numpy as np
 
-my_cv = my_pdf(a=0, b=1, name='my_pdf')
 
-# make an exponential distribution from 1 to KS =4, and sample from it
-# + percent escape..
+def gene_birth_death_with_escape_pdf():
+    
+    step_size = 0.01
+    decay_constant=3  # =(1/mean life expectancy of SSD gene)
+    rate_escape=0.1   # % of SSD are maintained
+    xs = np.arange(0, 4, step_size)
+
+    exp_decay_ys=[math.exp(-1*decay_constant*x) for x in xs]
+    ys_that_have_died=[1.0-math.exp(-1*decay_constant*x) for x in xs]
+    ys_that_escape=[y*rate_escape for y in ys_that_have_died]
+    ys_remaining=[exp_decay_ys[idx]+ys_that_escape[idx] for idx in range(0,len(exp_decay_ys))]
+
+    normalization_factor=1.0/(step_size*sum(ys_remaining))
+    normalized_pdf=[normalization_factor*y for y in ys_remaining]
+    return normalized_pdf
